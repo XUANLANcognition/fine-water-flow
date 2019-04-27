8e883a1c8451ef429f794d8230fff1c02777f1e5
xuanlan <guoliangxuanlan@outlook.com>
2019-04-28 00:46:43 +0800

    Notice.

--- fron/src/Notice.jsx
+++ fron/src/Notice.jsx
@@ -1,43 +1,20 @@
 import React, { Component } from 'react'
-import { Layout, Collapse } from 'antd'
+import { Layout, Timeline } from 'antd'
 
 import Nav from './Nav'
 import Myfooter from './Myfooter'
 
-const Panel = Collapse.Panel
-const title = ['1', '2']
-const text = ['1', '2']
-const customPanelStyle = {
-  background: '#f7f7f7',
-  borderRadius: 4,
-  marginBottom: 24,
-  border: 0,
-  overflow: 'hidden'
-}
-
 class Notice extends Component {
-  componentDidMount () {
-  }
-
-  state = {
-    collapsed: false
-  };
-
-  onCollapse = (collapsed) => {
-    console.log(collapsed)
-    this.setState({ collapsed })
-  }
-
   render () {
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Nav />
-        <div style={{ flex: '1 0 ', padding: '60px', backgroundColor: '#fff' }} >
-          <Collapse bordered={false} defaultActiveKey={['1']}>
-            <Panel header={title} key='1' style={customPanelStyle}>
-              <p>{text}</p>
-            </Panel>
-          </Collapse>
+        <div style={{ flex: '1 0 ', padding: '60px', backgroundColor: '#fff' }}>
+          <Timeline>
+            <Timeline.Item color='blue'>用户说：“要有头像.”</Timeline.Item>
+            <Timeline.Item color='blue'>基本文章发布功能完成</Timeline.Item>
+            <Timeline.Item color='green'>项目诞生了！！！</Timeline.Item>
+          </Timeline>,
         </div>
         <Myfooter />
       </Layout>
