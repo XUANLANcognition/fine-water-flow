0181c0a72c81a40c37db50088a68c68f87f5b80b
xuanlan <guoliangxuanlan@outlook.com>
2019-05-04 09:37:04 +0800

    显示关注的数据流

---
+++ fron/src/ArticleFollowList.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import { List, Button, Skeleton, message, Avatar } from 'antd'
+import axios from 'axios'
+import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
+
+const count = 8
+
+class ArticleFollowList extends Component {
+    state = {
+      data: [],
+      cache: [],
+      loading: false,
+      initLoading: true,
+      page: 1
+    }
+
+    componentDidMount = async (v) => {
+      await this.getArticleData()
+      this.setState(function (state) {
+        return { initLoading: false }
+      })
+    }
+
+    getArticleData = async (v) => {
+      try {
+        let config = {
+          headers: { 'Authorization': 'Token ' + window.localStorage.getItem('token') }
+        }
+        const response = await axios.get(
+          'https://guoliang.online:8080/api/articles/follow/?format=json' + '&page=' + this.state.page + '&page_size=' + count,
+          config
+        )
+        this.data = response.data.results
+        this.setState(function (state) {
+          return { data: response.data.results, cache: response.data.results }
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    onLoadMore = async (v) => {
+      this.setState(function (state) {
+        return {
+          loading: true,
+          data: this.data.concat([...new Array(count)].map(() => ({ loading: true, name: {} })))
+        }
+      })
+      try {
+        this.state.page = this.state.page + 1
+        let config = {
+          headers: { 'Authorization': 'Token ' + window.localStorage.getItem('token') }
+        }
+        const response = await axios.get(
+          'https://guoliang.online:8080/api/articles/follow/?format=json' + '&page=' + this.state.page + '&page_size=' + count,
+          config
+        )
+        if (response.status !== 404) {
+          const cache = this.state.cache.concat(response.data.results)
+          this.setState(function (state) {
+            return { cache: cache, data: cache, loading: false }
+          }, () => {
+            window.dispatchEvent(new window.Event('resize'))
+          })
+        } else {
+          message.error('No more article ^-^')
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    render () {
+      const { initLoading, loading, data } = this.state
+      const loadMore = !initLoading && !loading ? (
+        <div style={{
+          textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px'
+        }}
+        >
+          {(this.state.data.length > 0) && <Button onClick={this.onLoadMore}>loading more</Button>}
+        </div>
+      ) : null
+
+      return (
+        <List
+          itemLayout='vertical'
+          dataSource={data}
+          size='large'
+          loadMore={loadMore}
+          loading={initLoading}
+          renderItem={item => (
+            <List.Item>
+              <Skeleton avatar title={false} loading={item.loading} active>
+                <List.Item.Meta
+                  title={<Link to={((item.user && item.user.id) + '' === window.localStorage.getItem('user_id') ? '/profile/' : '/visit/') + (item.user && item.user.id)}>{item.user && item.user.username}</Link>}
+                  avatar={<Avatar icon='user' src={item.user && item.user.last_name} />}
+                  description={dayjs(item.pub_date).fromNow()}
+                />
+                <Link to={'/article/' + item.id}>
+                  <h3>{item.title}</h3>
+                </Link>
+              </Skeleton>
+            </List.Item>
+          )}
+        />
+      )
+    }
+}
+
+export default ArticleFollowList
--- fron/src/THome.jsx
+++ fron/src/THome.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import { Layout, Row, Col, Divider, Form, Affix } from 'antd'
+import { Layout, Row, Col, Tabs, Form, Affix } from 'antd'
 import { withRouter } from 'react-router'
 
 import Nav from './Nav'
 import Advertisement from './Advertisement'
 import ArticleList from './ArticleList'
+import ArticleFollowList from './ArticleFollowList'
 import Myfooter from './Myfooter'
 import ProfileCard from './ProfileCard'
+
+const TabPane = Tabs.TabPane
 
 class THome extends Component {
   state = {
@@ -26,11 +29,18 @@ class THome extends Component {
     return (
       <Layout style={{ minHeight: '100vh', backgroundColor: '#fff' }}>
         <Nav />
-        <Row style={{ flex: '1 0', padding: '60px' }}>
-          <Col xl={{ span: 14, offset: 1 }} lg={{ span: 14 }} xs={{ span: 24 }} style={{ paddingBottom: '20px' }}>
-            <ArticleList />
+        <Row style={{ flex: '1 0', paddingTop: '30px', paddingBottom: '30px' }}>
+          <Col xl={{ span: 12, offset: 3 }} xs={{ span: 22, offset: 1 }} style={{ paddingBottom: '20px' }}>
+            <Tabs defaultActiveKey='1'>
+              <TabPane tab='全部' key='1'>
+                <ArticleList />
+              </TabPane>
+              <TabPane tab='关注' key='2'>
+                <ArticleFollowList />
+              </TabPane>
+            </Tabs>
           </Col>
-          <Col xl={{ span: 8, offset: 1 }} lg={{ span: 10 }} xs={{ span: 24 }}>
+          <Col xl={{ span: 5, offset: 1 }} xs={{ span: 22, offset: 1 }}>
             <Affix offsetTop={0}>
               <ProfileCard callback={this.goToEditorPage} />
             </Affix>
