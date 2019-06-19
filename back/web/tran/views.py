31665e05857e26bc293a1eb7e3bde32e513df855
xuanlan <guoliangxuanlan@outlook.com>
2019-06-19 10:30:33 +0800

    Just a commit.

--- back/web/tran/views.py
+++ back/web/tran/views.py
@@ -50,6 +50,15 @@ class Publish(permissions.BasePermission):
             return permissions.IsAuthenticated
         return True
 
+class BookPublish(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            if permissions.IsAuthenticated:
+                return (request.user.profile.media_editor_auth == '审核通过')
+            else:
+                return False
+        return True
 
 # User API
 
@@ -364,14 +373,13 @@ class BookFilter(filters.FilterSet):
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all().order_by('-pub_date')
     serializer_class = BookSerializer
-    permission_classes = (Publish,)
+    permission_classes = (BookPublish,)
     pagination_class = BookPagination
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = BookFilter
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        serializer.save(tag=0)
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
