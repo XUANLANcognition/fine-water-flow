0d3832d5502436d10836713dabee59dc28988b4a
xuanlan <guoliangxuanlan@outlook.com>
2019-07-02 13:58:52 +0800

    增加底栏说明

--- fron/src/Myfooter.jsx
+++ fron/src/Myfooter.jsx
@@ -10,8 +10,20 @@ class Myfooter extends Component {
   render () {
     return (
       <Footer style={{ textAlign: 'center', backgroundColor: '#343a40', color: '#fff' }}>
-        <img src='/icon.png' style={{ width: '60px', height: '60px', margin: '0 20px 0 20px' }} />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div style={{ margin: '5px 0' }}>
+            <img src='/icon.png' style={{ width: '60px', height: '60px' }} />Fine Water Flow
+          </div>
+          <div style={{ margin: '5px 0' }}>
             Fine Water Flow ©2019 Created by XUANLAN
+          </div>
+          <div style={{ margin: '5px 0' }}>
+            若本站收录内容无意侵犯了贵公司版权，请给网页底部邮箱地址来信，我会及时处理和回复，谢谢合作
+          </div>
+          <div style={{ margin: '5px 0' }}>
+            站长邮箱 : guoliangxuanlan@outlook.com
+          </div>
+        </div>
       </Footer>
     )
   }
