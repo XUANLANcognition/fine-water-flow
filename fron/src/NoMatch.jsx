e042de8a7be280a88789d4a8248ceac4f35d9841
xuanlan <guoliangxuanlan@outlook.com>
2019-07-04 17:55:41 +0800

    优化404页面

--- fron/src/NoMatch.jsx
+++ fron/src/NoMatch.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
-import { Result, Button } from 'antd'
+import { Result, Button, Icon } from 'antd'
 import { Link } from 'react-router-dom'
+
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1242637_owf7reonlx8.js'
+})
 
 class NoMatch extends Component {
   render () {
@@ -8,10 +12,11 @@ class NoMatch extends Component {
       <div>
         <Result
           title='404'
+          icon={<IconFont type='icon-icon-test' style={{ fontSize: '320px' }} />}
           subTitle='Sorry, the page you visited does not exist.'
           extra={
             <Link to='/'>
-              <Button type='primary'>Back Home</Button>
+              <Button type='primary'>返回主页</Button>
             </Link>
           }
         />
