86a1849b52053a528e5a023c6a80058e9fe8a2a3
xuanlan <guoliangxuanlan@outlook.com>
2019-08-02 13:06:47 +0800

    F:通知显示数据化

--- fron/src/Notice.jsx
+++ fron/src/Notice.jsx
@@ -1,27 +1,103 @@
 import React, { Component } from 'react'
-import { Layout, Timeline, Row, Col } from 'antd'
+import { Layout, Row, Col, Button, List, Skeleton } from 'antd'
+import axios from 'axios'
+import dayjs from 'dayjs'
 
 import Nav from './Nav'
 import Myfooter from './Myfooter'
 
+const count = 8
+
 class Notice extends Component {
+  page = 1
+  state = {
+    data: [],
+    cache: [],
+    loading: false,
+    initLoading: true
+  }
+
+  componentDidMount = async (v) => {
+    await this.getNoticeData()
+    this.setState({ initLoading: false })
+  }
+
+  getNoticeData = async (v) => {
+    try {
+      const response = await axios.get(
+        'https://finewf.club:8080/api/notices/?format=json' + '&page=' + this.page + '&page_size=' + count
+      )
+      this.setState({ data: response.data.results, cache: response.data.results })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  onLoadMore = async (v) => {
+    await this.setState({
+      loading: true,
+      cache: this.state.data.concat([...new Array(count)].map(() => ({ loading: true, name: {} })))
+    })
+    try {
+      this.page = this.page + 1
+      const response = await axios.get(
+        'https://finewf.club:8080/api/notices/?format=json' + '&page=' + this.page + '&page_size=' + count
+      )
+      const temp1 = this.state.data
+      if (response.status === 200) {
+        const temp = this.state.data.concat(response.data.results)
+        this.setState(
+          { data: temp, cache: temp, loading: false }
+          , () => {
+            window.dispatchEvent(new window.Event('resize'))
+          })
+      } else {
+        this.setState({
+          cache: temp1
+        })
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render () {
+    const { initLoading, loading, cache, data } = this.state
+    const loadMore = !initLoading && !loading ? (
+      <div style={{
+        textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px'
+      }}
+      >
+        {(data.length > 0) && <Button onClick={this.onLoadMore}>加载更多</Button>}
+      </div>
+    ) : null
+
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Nav />
-        <div style={{ flex: '1 0 ', backgroundColor: '#fff', paddingTop: '30px' }}>
+        <div style={{ flex: '1 0 ', backgroundColor: '#fff' }}>
           <Row style={{ paddingTop: '30px', paddingBottom: '30px' }}>
             <Col xxl={{ span: 14, offset: 5 }} xl={{ span: 20, offset: 2 }} xs={{ span: 22, offset: 1 }}>
-              <Timeline>
-                <Timeline.Item color='blue'>🎉🎉🎉 书籍影视区功能基本完成 ( 2019.06.27 )</Timeline.Item>
-                <Timeline.Item color='blue'>🎉 关注功能正式上线. ( 2019.05.10 )</Timeline.Item>
-                <Timeline.Item color='blue'>🎂 正式更名为Fine Water Flow. ( 2019.05.09 )</Timeline.Item>
-                <Timeline.Item color='blue'>🎉 关注功能即将上线. </Timeline.Item>
-                <Timeline.Item color='blue'>🐱‍🏍 功能疯狂诞生中...</Timeline.Item>
-                <Timeline.Item color='blue'>用户说：“要有头像.”</Timeline.Item>
-                <Timeline.Item color='blue'>基本文章发布功能完成</Timeline.Item>
-                <Timeline.Item color='green'>🎂🎂🎂 项目诞生了！！！ ( 2018.11.29 )</Timeline.Item>
-              </Timeline>
+              <List
+                itemLayout='vertical'
+                dataSource={cache}
+                loadMore={loadMore}
+                loading={initLoading}
+                split={false}
+                renderItem={item => (
+                  <List.Item>
+                    <Skeleton title={false} loading={item.loading} active >
+                      <div style={{ display: 'flex', flexDirection: 'column', background: '#f7f7f7', boxShadow: '0 1px 3px rgba(26,26,26,.1)', borderRadius: '10px', padding: '20px 20px', fontSize: '16px' }}>
+                        <div style={{ color: '#1a1a1a', fontWeight: '600', fontSize: '18px', fontStretch: '100%', paddingBottom: '10px' }}>{item.tab}</div>
+                        <div style={{ color: '#646464', fontSize: '15px', paddingBottom: '10px' }}>
+                          {item.content}
+                        </div>
+                        <div>{item.release_date && dayjs(item.release_date).fromNow()}</div>
+                      </div>
+                    </Skeleton>
+                  </List.Item>
+                )}
+              />
             </Col>
           </Row>
         </div>
