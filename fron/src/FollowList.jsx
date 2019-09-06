736d6f146b224e22854cf83ee454cf31ab324805
xuanlan <guoliangxuanlan@outlook.com>
2019-09-06 12:57:57 +0800

    Optimized.

--- fron/src/FollowList.jsx
+++ fron/src/FollowList.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-import { List, Card, Avatar } from 'antd'
+import { List, Menu, Avatar, Button, Dropdown } from 'antd'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-const { Meta } = Card
+const menu = (
+  <Menu>
+    <Menu.Item>
+      1(即将)
+    </Menu.Item>
+    <Menu.Item>
+      2(即将)
+    </Menu.Item>
+    <Menu.Item>
+      3(即将)
+    </Menu.Item>
+  </Menu>
+)
 
 class FollowList extends Component {
   state = {
@@ -35,18 +47,28 @@ class FollowList extends Component {
     return (
       <List
         grid={{
-          gutter: 36, xs: 1, xl: 2
+          gutter: 12, xs: 1, xl: 2
         }}
         dataSource={this.state.data}
         renderItem={item => (
           <List.Item>
-            <Card style={{ borderRadius: '1px', boxShadow: '0 1px 3px rgba(26,26,26,.1)' }}>
-              <Meta
-                avatar={<Link to={(item.id + '' === window.localStorage.getItem('user_id') ? '/profile/' : '/visit/') + (item.id)}><Avatar shape='square' src={item.last_name} /></Link>}
-                title={<Link to={(item.id + '' === window.localStorage.getItem('user_id') ? '/profile/' : '/visit/') + (item.id)}>{item.username}</Link>}
-                description={item.bio}
-              />
-            </Card>
+            <div style={{ display: 'flex', justifyContent: 'space-around', borderRadius: '3px', backgroundColor: '#f2f2f5', padding: '5px' }}>
+              <div style={{ borderRight: '1px solid rgb(217, 217, 217)', display: 'flex', flexDirection: 'column', justifyContent: 'center', margin: '10px 10px 10px 0', padding: '20px' }}>
+                <Link to={(item.id + '' === window.localStorage.getItem('user_id') ? '/profile/' : '/visit/') + (item.id)}>
+                  <Avatar size={35} shape='square' src={item.last_name} />
+                </Link>
+              </div>
+              <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'stretch' }}>
+                <div style={{ color: '#333', fontSize: '16px', fontWeight: 'bold' }}>{item.username}</div>
+                <div>{item.bio}</div>
+                <div style={{ display: 'flex', padding: '10px 0' }}>
+                  <Dropdown overlay={menu} placement='bottomRight'>
+                    <Button size='small' style={{ marginRight: '10px' }}>分组(即将)</Button>
+                  </Dropdown>
+                  <Button size='small'>取消关注(即将)</Button>
+                </div>
+              </div>
+            </div>
           </List.Item>
         )}
       />
