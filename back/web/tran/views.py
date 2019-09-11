f0fa367eb594d63889746f24c88cd776d93d0516
xuanlan <guoliangxuanlan@outlook.com>
2019-09-11 12:52:47 +0800

    Fix bug.

--- back/web/tran/views.py
+++ back/web/tran/views.py
@@ -123,6 +123,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             profile_data = validated_data.pop('profile')
             profile = instance.profile
             profile.bio = profile_data.get('bio', profile.bio)
+            profile.profession = profile.get('profession', profile.profession)
             profile.save()
         return super(UserSerializer, self).update(instance, validated_data)
 
