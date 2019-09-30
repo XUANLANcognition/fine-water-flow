9af25de102cea2a1dd5a60e1fecaaba9a0deb804
wda43779 <wda43779@gmail.com>
2019-09-30 19:28:04 +0800

    定制 antd 的主题

--- fron/config-overrides.js
+++ fron/config-overrides.js
@@ -1,10 +1,15 @@
-const { injectBabelPlugin } = require('react-app-rewired')
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
 
-module.exports = function override (config, env) {
-  // do stuff with the webpack config...
-  config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
-    config
-  )
-  return config
-}
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      '@primary-color': '#198fff',
+    }
+  })
+)
