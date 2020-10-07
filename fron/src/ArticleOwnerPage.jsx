f712ee9ea10c6ad9733f50f6dfc22ceb5639782a
xuanlan <guoliangxuanlan@outlook.com>
2020-10-08 01:29:22 +0800

    大幅改进文章阅读页面布局

--- fron/src/ArticleOwnerPage.jsx
+++ fron/src/ArticleOwnerPage.jsx
@@ -1,40 +1,53 @@
-import React, { Component } from 'react'
-import { Row, Col, Layout, Form, Spin, Affix, Typography, BackTop, Statistic } from 'antd'
-import axios from 'axios'
-import dayjs from 'dayjs'
-import 'braft-editor/dist/output.css'
+import React, { Component } from "react";
+import {
+  Row,
+  Col,
+  Layout,
+  Form,
+  Spin,
+  Affix,
+  Typography,
+  BackTop,
+  Statistic,
+} from "antd";
+import axios from "axios";
+import dayjs from "dayjs";
+import "braft-editor/dist/output.css";
 
-import Nav from './Nav'
-import Myfooter from './Myfooter'
-import AuthorShowCard from './AuthorShowCard'
-import Advertisement from './Advertisement'
-import AddComment from './AddComment'
+import Nav from "./Nav";
+import Myfooter from "./Myfooter";
+import AuthorShowCard from "./AuthorShowCard";
+import Advertisement from "./Advertisement";
+import AddComment from "./AddComment";
+import AvatarF from "./AvatarF";
+import LikeButton from "./components/LikeButton";
 
-const { Paragraph } = Typography
-const { Title } = Typography
+const { Paragraph } = Typography;
+const { Title } = Typography;
 
-class ArticleOwnerPage extends Component {
-  constructor (props) {
-    super(props)
+class ArticlePage extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      title: 'loading...',
-      content: '',
-      id: '',
-      url: '',
+      title: "loading...",
+      content: "",
+      id: "",
+      url: "",
       translationList: [],
       visible: false,
-      modalTitle: '',
-      modalContent: '',
+      modalTitle: "",
+      modalContent: "",
       loading: true,
-      authorId: '',
-      pubDate: '',
-      views: 0
-    }
+      authorId: "",
+      pubDate: "",
+      views: 0,
+      user: null,
+    };
   }
 
   componentDidMount = async (v) => {
-    await this.getArticle()
-  }
+    await this.getArticle();
+  };
 
   getArticle = async (v) => {
     try {
@@ -52,62 +65,172 @@ class ArticleOwnerPage extends Component {
           loading: false,
           authorId: response.data.user.id,
           pubDate: response.data.pub_date,
-          views: response.data.views
-        }
-      })
+          views: response.data.views,
+          user: response.data.user,
+        };
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   handleOk = (e) => {
-    console.log(e)
+    console.log(e);
     this.setState({
-      visible: false
-    })
-  }
+      visible: false,
+    });
+  };
 
   handleCancel = (e) => {
-    console.log(e)
+    console.log(e);
     this.setState({
-      visible: false
-    })
-  }
+      visible: false,
+    });
+  };
 
-  render () {
+  render() {
     return (
-      <Layout style={{ minHeight: '100vh', backgroundColor: '#fff' }}>
+      <Layout style={{ minHeight: "100vh", backgroundColor: "#fff" }}>
         <Nav />
         <BackTop />
-        <Row style={{ background: '#fff', padding: '20px 0', marginBottom: '15px', boxShadow: '0px 2px 2px #888888' }}>
-          <Col xxl={{ span: 10, offset: 5 }} xl={{ span: 13, offset: 2 }} md={{ span: 14, offset: 1 }} xs={{ span: 22, offset: 1 }}>
-            <div style={{ fontSize: '22px', fontWeight: 'bold', color: 'black' }}>
-              <Paragraph ellipsis={{ rows: 1, expandable: true }} strong style={{ color: 'black' }}>
-                {this.state.title}
-              </Paragraph>
-            </div>
-          </Col>
-          <Col xxl={{ span: 4, offset: 0 }} xl={{ span: 6, offset: 1 }} md={{ span: 7, offset: 1 }} xs={{ span: 22, offset: 1 }} style={{ paddingLeft: '15px' }}>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-              <Statistic title='浏览次数' suffix='次' value={this.state.views} />
-              <Statistic title='发布时间' value={dayjs(this.state.pubDate).fromNow()} />
-            </div>
-          </Col>
-        </Row>
-        <Row style={{ flex: '1 0' }}>
-          <Col xxl={{ span: 10, offset: 5 }} xl={{ span: 14, offset: 2 }} md={{ span: 15, offset: 1 }} xs={{ span: 22, offset: 1 }}>
-            <div type='flex' style={{ flex: '1 0', background: '#fff', padding: '10px 0', marginBottom: '20px' }}>
-              <div style={{ fontSize: '16px' }}>
-                <div className='braft-output-content' style={{ overflow: 'auto' }} dangerouslySetInnerHTML={{ __html: this.state.content }} />
+        <Row style={{ flex: "1 0", marginTop: "26px" }}>
+          <Col
+            xxl={{ span: 10, offset: 5 }}
+            xl={{ span: 14, offset: 2 }}
+            md={{ span: 15, offset: 1 }}
+            xs={{ span: 22, offset: 1 }}
+          >
+            <div
+              type="flex"
+              style={{
+                background: "#fff",
+                marginBottom: "20px",
+              }}
+            >
+              <div
+                style={{
+                  border: "2px solid #8c8c8c",
+                  borderRadius: "6px",
+                  padding: "16px 18px",
+                  marginBottom: "22px",
+                }}
+              >
+                <div
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "bold",
+                    color: "#000",
+                    marginBottom: "8px",
+                  }}
+                >
+                  {this.state.title}
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginTop: "18px",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <div>
+                    {" "}
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div
+                        style={{
+                          fontSize: "16px",
+                          marginRight: "8px",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        作者 :{" "}
+                      </div>
+                      <AvatarF user={this.state.user}></AvatarF>
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row-reverse",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div
+                      style={{
+                        fontSize: "18px",
+                        fontWeight: "bold",
+                        marginLeft: "26px",
+                      }}
+                    >
+                      {"发布于 " + dayjs(this.state.pubDate).fromNow()}
+                    </div>
+                    <div style={{ fontSize: "18px", fontWeight: "bold" }}>
+                      {"被浏览 " + this.state.views + " 次"}
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div style={{ textAlign: 'center' }}>
-                <Spin spinning={this.state.loading} size='large' tip='loading...' />
+
+              <div
+                style={{
+                  fontSize: "16px",
+                  border: "1px solid #8c8c8c",
+                  borderRadius: "6px",
+                  padding: "16px 12px",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    alignContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    className="braft-output-content"
+                    style={{
+                      overflow: "auto",
+                    }}
+                    dangerouslySetInnerHTML={{ __html: this.state.content }}
+                  />
+                </div>
+
+                <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+                  <LikeButton article_id={this.state.id}></LikeButton>
+                </div>
               </div>
-              <Title level={4} style={{ paddingTop: '20px' }}>评论 · · · · · ·</Title>
-              <AddComment articleId={this.state.id} articleUrl={this.state.url} />
+              <div style={{ textAlign: "center" }}>
+                <Spin
+                  spinning={this.state.loading}
+                  size="large"
+                  tip="loading..."
+                />
+              </div>
+              <Title level={4} style={{ paddingTop: "20px" }}>
+                评论 · · · · · ·
+              </Title>
+              <AddComment
+                articleId={this.state.id}
+                articleUrl={this.state.url}
+              />
             </div>
           </Col>
-          <Col xxl={{ span: 4, offset: 0 }} xl={{ span: 6, offset: 0 }} md={{ span: 7, offset: 0 }} xs={{ span: 22, offset: 1 }} style={{ paddingBottom: '20px', paddingLeft: '15px' }}>
+          <Col
+            xxl={{ span: 4, offset: 0 }}
+            xl={{ span: 6, offset: 0 }}
+            md={{ span: 7, offset: 0 }}
+            xs={{ span: 22, offset: 1 }}
+            style={{ paddingBottom: "20px", paddingLeft: "15px" }}
+          >
             <Affix offsetTop={0}>
               <AuthorShowCard authorId={this.state.authorId} />
             </Affix>
@@ -116,8 +239,8 @@ class ArticleOwnerPage extends Component {
         </Row>
         <Myfooter />
       </Layout>
-    )
+    );
   }
 }
 
-export default Form.create()(ArticleOwnerPage)
+export default Form.create()(ArticlePage);
